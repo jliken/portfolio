a994c3ad6b5ae3778b01e3d108005bad1f8ff8ef
jliken <stripytiger2005@gmail.com>
2026-04-17 22:48:17 +0100

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Jack Liken's Portfolio";
+export const SITE_TITLE = "Jack Liken's";
 export const SITE_DESCRIPTION = "Welcome to my website!";
